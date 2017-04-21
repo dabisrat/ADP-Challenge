b0ac9ccff4661c691bfd71388d2dc38e316b4880
David Bisrat <dabisrat@gmail.com>
2017-04-21 10:47:16 -0400

    (refactor) re-organize the file structure and app naming convention

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {RouterModule} from '@angular/router';
+import {Route, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductPageComponent } from './product-page/product-page.component';
-import {HttpMethodsService} from './http-methods.service';
-import { AddProductComponent } from './add-product/add-product.component';
-import { EditProductComponent } from './edit-product/edit-product.component';
 
-const routes = [
- {path: '', component: WelcomeComponent},
- {path: 'products', component: ProductListComponent},
- {path: 'product',  component: ProductPageComponent}
+
+const routes: Route[] = [
+ {path: '', component: },
+ {path: 'products', component: },
+ 
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    WelcomeComponent,
-    ProductListComponent,
-    ProductPageComponent,
-    AddProductComponent,
-    EditProductComponent
+
   ],
   imports: [
     BrowserModule,
