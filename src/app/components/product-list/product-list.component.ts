ac6b1aa0969481d3e57dca580cc376b0c0b82cb4
David Bisrat <dabisrat@gmail.com>
2017-04-22 14:25:47 -0400

    feat: complete functionality  on list page

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -3,6 +3,7 @@ import { Product } from './../../interfaces/product-model';
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -10,27 +11,44 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class ProductListComponent implements OnInit {
   products: Product[];
+  isCreating = false;
+  newProductName: string;
+  newProductDescription: string;
   constructor( private router: Router, private route: ActivatedRoute, private productService: ProductService) { }
 
   ngOnInit() {
      this.getProducts();
   }
-  getProducts() {
-    this.productService.getAllProducts()
+  getProducts(): void {
+     this.productService.getAllProducts()
                        .then(products => this.products = products)
                        .catch(error => console.error(error));
   }
 
-  deleteProduct(event: Event, product: Product) {
+  deleteProduct(event: Event, product: Product): void {
     event.stopPropagation();
-    this.productService.deleteProduct(product.id)
+     this.productService.deleteProduct(product.id)
                        .then(() => this.products = this.products.filter( currentProduct => currentProduct !== product ) )
                        .catch(error => console.error(error));
   }
 
-  selectProduct(product: Product) {
+  selectProduct(product: Product): void {
     this.router.navigate( ['products', product.id] );
   }
 
+  addProduct() {
+    const id = this.products.length + 1;
+    const newProduct = Object.assign({}, {id: id}, {name: this.newProductName, description: this.newProductDescription});
+    this.productService.addProduct(newProduct)
+                       .then( () => this.newProductDescription = this.newProductName = '')
+                       .then( () => this.products.push(newProduct) )
+                       .catch(error => console.error);
+  }
+  updateProduct(product: Product) {
 
+  }
+
+  toggleState(event: Event) {
+    this.isCreating = !this.isCreating;
+  }
 }
