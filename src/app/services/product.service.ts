a5cfe8d3d7a7885abefc72751be61ca28975cc28
David Bisrat <dabisrat@gmail.com>
2017-04-22 14:27:19 -0400

    feat: add CRUD functionality to the service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,13 +1,12 @@
 import { Product } from './../interfaces/product-model';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-// import 'rxjs/add/operator/catch';
-// import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
+
 const baseUrl  = `http://localhost:3000/product/`;
 const headers = new Headers({ 'Content-Type': 'application/json' });
 const options = new RequestOptions({ headers: headers });
+
 @Injectable()
 export class ProductService {
 
@@ -21,23 +20,33 @@ export class ProductService {
   };
 
 
-  getProdutById(productId: number) {
+  getProdutById(productId: number): Promise<any> {
   return this.http.get(baseUrl + productId)
-             .toPromise()
-             .then(this.processData )
-             .catch(this.handelError);
+                  .toPromise()
+                  .then(this.processData )
+                  .catch(this.handelError);
   }
 
-  deleteProduct(productId: number) {
+  deleteProduct(productId: number): Promise<null> {
    return this.http.delete(baseUrl + productId, options)
-              .toPromise()
-              .then(() => null)
-              .catch(this.handelError);
+                   .toPromise()
+                   .then(() => console.log(`product ${productId} deleted`))
+                   .catch(this.handelError);
   }
 
-  // updateProduct(product: Product) {}
+  updateProduct(product: Product) {
+    return this.http.put(baseUrl + product.id, JSON.stringify(product), options)
+                    .toPromise()
+                    .then(this.processData)
+                    .catch(this.handelError);
+}
 
-  // addProduct(product: Product) {}
+  addProduct(product: Product) {
+    return this.http.post(baseUrl, JSON.stringify(product), options)
+                    .toPromise()
+                    .then(this.processData)
+                    .catch(this.handelError);
+  }
 
   processData(res: Response): Product[] {
     console.log(res.json());
